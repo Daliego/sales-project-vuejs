d915c6ea29c4ae3cab15627d64720534c87274f7
Diego Araújo <diesgo.araujo@gmail.com>
2024-06-21 20:53:29 -0300

    feat: layout and not found component for search errors

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import NavBar from '@/components/shared/NavBar.vue'
+import { RouterView } from 'vue-router';
+</script>
+<template>
+  <div class="w-full h-full gradient">
+    <RouterView />
+    <NavBar />
+  </div>
+</template>
